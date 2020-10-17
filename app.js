d89ffbd379288cbff181cb3145699585c8eb4362
Marvin <marvinfrancisdale@gmail.com>
2020-10-17 18:20:57 +0100

    add cursor, add monospaced font

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const green   = "limegreen";
 let grey    = "grey"
 
-const words = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at"];
+const commonWords = ["the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"];
 
-//const words = ["the"];
+const numOfWords = 30;
+
+words = getRandomWords(commonWords, numOfWords);
 
 let mins = 0;
- //gross wpm formula (words.length / 5) / mins;
+ 
 
  let totalChars = displayWords(words);
  console.log("Total Chars " + totalChars);
@@ -16,14 +18,12 @@ let text = document.querySelectorAll(".typeable");
 let i = 0;
 let textLenght = text.length - 1;
 
-highlightText(text, i, grey);
+//highlightText(text, i, grey);
+addCursor(text, i);
 
 document.addEventListener("keypress", (event) => {
 
-    if(i === 0){
-      
-      let startTime = startTimer();
-    }
+    if(i === 0) { let startTime = startTimer();}
 
     let text = document.querySelectorAll(".typeable");
     let textLenght = text.length -1;
@@ -32,27 +32,40 @@ document.addEventListener("keypress", (event) => {
     if (event.key === key) {
       grey = "grey"
       highlightText(text, i, green);
+      removeCursor(text, i);
       i++;
   
-      if (i === textLenght) {
+        if (i === textLenght) {
 
-      let gWPM = getWpm(startTime, totalChars);
-      document.querySelector(".wpm").innerHTML = gWPM.toPrecision(2);
-        
-      i = restart();
-      reset();
-      } 
+          let gWPM = getWpm(startTime, totalChars);
+          document.querySelector(".wpm").innerHTML = gWPM.toPrecision(2);
+
+          i = restart();
+          reset();
+        } 
       } else {
-        grey = "red"
-        highlightText(text, i, grey);
+
+      grey = "red"
+      addCursor(text, i);
+      highlightText(text, i, grey);
       
       
     }
-     highlightText(text, i, grey);
+    addCursor(text, i);
+     
+     
   });
 
 function highlightText(text, i, color) {
-  text[i].style.backgroundColor = color;
+  text[i].style.color = color;
+}
+
+function addCursor(text, i) {
+  text[i].classList.add("blinking-cursor");
+}
+
+function removeCursor(text, i) {
+  text[i].classList.remove("blinking-cursor");
 }
 
 function getChar(text, i) {
@@ -75,11 +88,12 @@ function restart() {
 }
 
 function reset () {
+  words = getRandomWords(commonWords, numOfWords);
   displayWords(words);
   let text = document.querySelectorAll(".typeable");
   let i = 0;
   
-  highlightText(text, i, grey);
+  addCursor(text, i);
 }
 
 function displayWords(words) {
@@ -119,5 +133,21 @@ function getWpm(startTime, totalChars) {
   ellapsedTime /= 1000; //get seconds
   ellapsedTime /= 60; //get minutes
 
+  //gross wpm formula (words.length / 5) / mins;
   return (totalChars / 5) / ellapsedTime;
+}
+
+
+function getRandomWords(arr, n) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
 }
